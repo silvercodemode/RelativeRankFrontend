0032dc32601259aecb158509017f0e389fa07878
Martin <martinpayne73@gmail.com>
2020-04-28 04:31:11 -0400

    update favicons, add manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,25 @@ function App({ Component, pageProps }) {
     <Provider store={store}>
       <Head>
         <title>Relative Rank</title>
-        <link rel="icon" type="image/x-icon" href="/images/favicon.png" />
+        <link rel="icon" href="/images/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Component {...pageProps} />
     </Provider>
