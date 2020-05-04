aeccf1f2ee3dcef46bbebe91b18be81e4475b420
Martin <martinpayne73@gmail.com>
2020-05-04 17:20:08 -0400

    add move to top/bottom buttons on shows, indicate unsaved changes with red outline on list

--- components/DraggableRankedShow.tsx
+++ components/DraggableRankedShow.tsx
@@ -1,16 +1,20 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useRef } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import { RelativeRankedShow as RelativeRankedShowType } from '../redux/store';
 
 type ShowWithUpdatorFunction = {
   show: RelativeRankedShowType;
   updator: (name: string) => void;
+  moveToTop: (name: string) => void;
+  moveToBottom: (name: string) => void;
 };
 
 export default function DraggableRankedShow({
   show,
   updator,
+  moveToTop,
+  moveToBottom,
 }: ShowWithUpdatorFunction) {
   const { name, rank, percentileRank } = show;
 
@@ -32,14 +36,36 @@ export default function DraggableRankedShow({
             </span>
             <div className="flex flex-1 justify-end">
               <span className="text-xl">{name}</span>
-              <button
-                type="button"
-                className="p-1 ml-2 rounded-lg shadow-inner hover:shadow-md"
-                onClick={() => updator(name)}
-              >
-                Delete
-              </button>
             </div>
+          </div>
+          <div className="p-2">
+            <button
+              type="button"
+              className={`p-1 ml-2 rounded-lg shadow-md hover:bg-${
+                rank % 2 === 1 ? 'gray-100' : 'green-700'
+              }`}
+              onClick={() => moveToTop(name)}
+            >
+              Move to Top
+            </button>
+            <button
+              type="button"
+              className={`p-1 ml-2 rounded-lg shadow-md hover:bg-${
+                rank % 2 === 1 ? 'gray-100' : 'green-700'
+              }`}
+              onClick={() => moveToBottom(name)}
+            >
+              Move to Bottom
+            </button>
+            <button
+              type="button"
+              className={`p-1 ml-2 rounded-lg shadow-md hover:bg-${
+                rank % 2 === 1 ? 'gray-100' : 'green-700'
+              }`}
+              onClick={() => updator(name)}
+            >
+              Delete
+            </button>
           </div>
         </div>
       )}
--- components/UserShowList.tsx
+++ components/UserShowList.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-nested-ternary */
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import { throttle } from 'lodash';
@@ -44,7 +45,7 @@ function searchOnType(searchToExecute, sendSearch) {
     if (searchToExecute && searchDelay.readyToSearch === symbol) {
       sendSearch(searchToExecute);
     }
-  }, 1500);
+  }, 1000);
 }
 
 export default function UserShowList({
@@ -61,9 +62,18 @@ export default function UserShowList({
 
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [username, setUsername] = useState('');
+  const [listHasUnsavedChanges, setListHasUnsavedChanges] = useState<boolean>(
+    false,
+  );
+  const [isDragging, setIsDragging] = useState<boolean>(false);
   const dispatch = useDispatch();
 
+  function onDragStart() {
+    setIsDragging(true);
+  }
+
   function onDragEnd({ source, destination }: dragResult) {
+    setIsDragging(false);
     if (destination && destination.droppableId === 'user-show-list') {
       const updatedShows = [...shows];
 
@@ -82,7 +92,39 @@ export default function UserShowList({
       }
 
       setShowList(updatedShows);
+      setListHasUnsavedChanges(true);
     }
+  }
+
+  function moveToTop(showName: string) {
+    const show = shows.find((s) => s.name === showName);
+    const otherShows = shows.filter((s) => s.name !== showName);
+    let updatedShows = otherShows ? [show, ...otherShows] : [show];
+    updatedShows = updatedShows.map((s, i) => ({
+      name: s.name,
+      rank: i + 1,
+      percentileRank: 1 - (1 / (shows.length + 1)) * (i + 1),
+    }));
+    setShowList(updatedShows);
+    setListHasUnsavedChanges(true);
+
+    const y = window.scrollY;
+    setTimeout(() => {
+      window.scrollTo(0, y);
+    }, 0);
+  }
+
+  function moveToBottom(showName: string) {
+    const show = shows.find((s) => s.name === showName);
+    const otherShows = shows.filter((s) => s.name !== showName);
+    let updatedShows = otherShows ? [...otherShows, show] : [show];
+    updatedShows = updatedShows.map((s, i) => ({
+      name: s.name,
+      rank: i + 1,
+      percentileRank: 1 - (1 / (shows.length + 1)) * (i + 1),
+    }));
+    setShowList(updatedShows);
+    setListHasUnsavedChanges(true);
   }
 
   function addShowToTop(showName) {
@@ -100,6 +142,7 @@ export default function UserShowList({
       percentileRank: 1 - (1 / (shows.length + 2)) * (i + 1),
     }));
     setShowList(updatedShows);
+    setListHasUnsavedChanges(true);
   }
 
   function addShowToBottom(showName) {
@@ -117,6 +160,7 @@ export default function UserShowList({
       percentileRank: 1 - (1 / (shows.length + 2)) * (i + 1),
     }));
     setShowList(updatedShows);
+    setListHasUnsavedChanges(true);
   }
 
   function deleteShow(showName) {
@@ -132,6 +176,7 @@ export default function UserShowList({
         percentileRank: 1 - (1 / shows.length) * (i + 1),
       }));
     setShowList(updatedShows);
+    setListHasUnsavedChanges(true);
   }
 
   function clearSearch() {
@@ -162,6 +207,7 @@ export default function UserShowList({
       showList = await response.json();
     } finally {
       setShowList(showList);
+      setListHasUnsavedChanges(true);
       dispatch(receiveImportFromMal());
     }
   }
@@ -173,141 +219,31 @@ export default function UserShowList({
   }
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
+    <DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart}>
       <Droppable droppableId="user-show-list">
         {(provided) => (
-          <main
-            className="max-w-xl m-5 mx-auto text-center"
-            ref={provided.innerRef}
-          >
-            <div className="rounded-lg shadow-md my-2 py-2">
-              <h4>Add Show</h4>
-              <div className="pl-5 pr-5 flex flex-wrap content-center justify-between">
-                <label htmlFor="search" className="text-lg m-2 pt-2">
-                  Search
-                </label>
-                <input
-                  id="search"
-                  type="text"
-                  value={searchTerm}
-                  className="flex-grow border-solid border-4 m-2 p-1 rounded focus:outline-none focus:shadow-outline"
-                  onChange={(event) => {
-                    setSearchTerm(event.target.value);
-                    searchOnType(event.target.value, sendSearch);
-                  }}
-                  onKeyPress={onKeyPress}
-                />
-                <div className="text-center min-w-full sm:min-w-0">
-                  <button
-                    type="button"
-                    className="m-2 max-w-xs bg-green-800 hover:bg-green-700 text-white text-lg py-2 px-4 rounded flex-grow-0"
-                    onClick={sendSearch}
-                  >
-                    Search
-                  </button>
-                </div>
-              </div>
-              {(searchExecuted || searchResultsLoading) &&
-                (searchResultsLoading ? (
-                  <div className="mb-4">
-                    <LoadingSpinner />
-                  </div>
-                ) : (
-                  <>
+          <>
+            {!isDragging &&
+              !userShowsLoading &&
+              (listHasUnsavedChanges || shows.length > 10) && (
+                <div className="fixed content-center justify-center bottom-0 w-screen bg-white flex text-center">
+                  {shows.length > 10 && (
                     <button
                       type="button"
-                      className="mx-auto p-1 max-w-xs bg-green-800 hover:bg-green-700 text-white text-xs rounded"
-                      onClick={clearSearch}
+                      className="m-4 p-2 rounded-lg shadow-md hover:bg-gray-100"
+                      onClick={() => {
+                        window.scrollTo({
+                          top: 0,
+                        });
+                      }}
                     >
-                      Clear Search Results
+                      Go to Top
                     </button>
-                    {searchResults.map && searchResults.length < 1 ? (
-                      <p>No shows matched your search.</p>
-                    ) : (
-                      searchResults.map((show: RelativeRankedShow) => (
-                        <div
-                          key={show.name}
-                          className="flex justify-between pl-5 pr-5"
-                        >
-                          <p className="pt-2 max-w-xs">{show.name}</p>
-                          <div>
-                            <button
-                              type="button"
-                              className="m-2 p-1 max-w-xs bg-green-800 hover:bg-green-700 text-white text-xs rounded"
-                              onClick={() => addShowToTop(show.name)}
-                            >
-                              Add To Top
-                            </button>
-                            <button
-                              type="button"
-                              className="m-2 p-1 max-w-xs bg-green-800 hover:bg-green-700 text-white text-xs rounded"
-                              onClick={() => addShowToBottom(show.name)}
-                            >
-                              Add To Bottom
-                            </button>
-                          </div>
-                        </div>
-                      ))
-                    )}
-                  </>
-                ))}
-            </div>
-            <div className="pl-5 pr-5 flex content-center justify-between">
-              <p className="mt-3 text-lg text-left">
-                Drag and drop to reorder list.
-              </p>
-              <button
-                type="button"
-                className="m-2 max-w-xs bg-green-800 hover:bg-green-700 text-white text-lg py-2 px-4 rounded flex-grow-0"
-                onClick={() => dispatch(updateUserShowList(shows))}
-              >
-                Save
-              </button>
-            </div>
-            {userShowsLoading ? (
-              <div className="mt-20">
-                <LoadingSpinner />
-              </div>
-            ) : shows.length < 1 ? (
-              <>
-                <p className="m-3">No shows yet.</p>
-                <div className="m-5 shadow-md p-3 rounded-lg flex flex-col justify-center items-center">
-                  <h2 className="text-center mb-1 text-xl">
-                    Import from MyAnimeList?
-                  </h2>
-                  <p className="text-center text-xs">
-                    (MAL account must be public)
-                  </p>
-                  <div className="flex-auto flex justify-between">
-                    <label htmlFor="username" className="text-lg m-2">
-                      MAL Username
-                    </label>
-                    <input
-                      id="username"
-                      type="text"
-                      value={username}
-                      className="flex-grow border-solid border-4 m-2 p-1 rounded focus:outline-none focus:shadow-outline"
-                      onChange={(event) => setUsername(event.target.value)}
-                      onKeyPress={onKeyPressImportFromMal}
-                    />
-                  </div>
-
-                  <button
-                    type="button"
-                    className="m-2 max-w-xs bg-green-800 hover:bg-green-700 text-white text-lg py-2 px-4 rounded flex-grow-0"
-                    onClick={() => importFromMal()}
-                  >
-                    Import
-                  </button>
-                </div>
-              </>
-            ) : (
-              <>
-                {shows.length > 10 && (
-                  <div className="max-w-xl mb-5 mx-auto text-center">
+                  )}
+                  {shows.length > 10 && (
                     <button
                       type="button"
-                      className="p-2 rounded-lg shadow-md"
+                      className="m-4 p-2 rounded-lg shadow-md hover:bg-gray-100"
                       onClick={() => {
                         window.scrollTo({
                           top: document.body.scrollHeight,
@@ -316,34 +252,159 @@ export default function UserShowList({
                     >
                       Go To Bottom
                     </button>
-                  </div>
-                )}
-                {shows.map((show) => (
-                  <DraggableRankedShow
-                    key={show.name}
-                    show={show}
-                    updator={deleteShow}
-                  />
-                ))}
-                {shows.length > 10 && (
-                  <footer className="max-w-xl m-5 mx-auto text-center">
+                  )}
+                  {listHasUnsavedChanges && (
                     <button
                       type="button"
-                      className="m-4 p-2 rounded-lg shadow-md"
+                      className="m-4 bg-red-700 hover:bg-red-600 text-white text-lg py-2 px-4 rounded"
                       onClick={() => {
-                        window.scrollTo({
-                          top: 0,
-                        });
+                        dispatch(updateUserShowList(shows));
+                        setListHasUnsavedChanges(false);
                       }}
                     >
-                      Back to Top
+                      Save Changes
                     </button>
-                  </footer>
-                )}
-              </>
-            )}
-            {provided.placeholder}
-          </main>
+                  )}
+                </div>
+              )}
+            <main
+              className="max-w-xl mx-5 mt-5 mb-20 mx-auto text-center"
+              ref={provided.innerRef}
+            >
+              <div className="rounded-lg shadow-md my-2 py-2">
+                <h4>Add Show</h4>
+                <div className="pl-5 pr-5 flex flex-wrap content-center justify-between">
+                  <label htmlFor="search" className="text-lg m-2 pt-2">
+                    Search
+                  </label>
+                  <input
+                    id="search"
+                    type="text"
+                    value={searchTerm}
+                    className="flex-grow border-solid border-4 m-2 p-1 rounded focus:outline-none focus:shadow-outline"
+                    onChange={(event) => {
+                      setSearchTerm(event.target.value);
+                      searchOnType(event.target.value, sendSearch);
+                    }}
+                    onKeyPress={onKeyPress}
+                  />
+                  <div className="text-center min-w-full sm:min-w-0">
+                    <button
+                      type="button"
+                      className="m-2 max-w-xs bg-green-800 hover:bg-green-700 text-white text-lg py-2 px-4 rounded flex-grow-0"
+                      onClick={sendSearch}
+                    >
+                      Search
+                    </button>
+                  </div>
+                </div>
+                {(searchExecuted || searchResultsLoading) &&
+                  (searchResultsLoading ? (
+                    <div className="mb-4">
+                      <LoadingSpinner />
+                    </div>
+                  ) : (
+                    <>
+                      <button
+                        type="button"
+                        className="mx-auto p-1 max-w-xs bg-green-800 hover:bg-green-700 text-white text-xs rounded"
+                        onClick={clearSearch}
+                      >
+                        Clear Search Results
+                      </button>
+                      {searchResults.map && searchResults.length < 1 ? (
+                        <p>No shows matched your search.</p>
+                      ) : (
+                        searchResults.map((show: RelativeRankedShow) => (
+                          <div
+                            key={show.name}
+                            className="flex justify-between pl-5 pr-5"
+                          >
+                            <p className="pt-2 max-w-xs">{show.name}</p>
+                            <div>
+                              <button
+                                type="button"
+                                className="m-2 p-1 max-w-xs bg-green-800 hover:bg-green-700 text-white text-xs rounded"
+                                onClick={() => addShowToTop(show.name)}
+                              >
+                                Add To Top
+                              </button>
+                              <button
+                                type="button"
+                                className="m-2 p-1 max-w-xs bg-green-800 hover:bg-green-700 text-white text-xs rounded"
+                                onClick={() => addShowToBottom(show.name)}
+                              >
+                                Add To Bottom
+                              </button>
+                            </div>
+                          </div>
+                        ))
+                      )}
+                    </>
+                  ))}
+              </div>
+              <div className="pl-5 pr-5 flex content-center justify-center text-center">
+                <p className="m-3 text-lg">Drag and drop to reorder list.</p>
+              </div>
+              {userShowsLoading ? (
+                <div className="mt-20">
+                  <LoadingSpinner />
+                </div>
+              ) : shows.length < 1 ? (
+                <>
+                  <p className="m-3">No shows yet.</p>
+                  <div className="m-5 shadow-md p-3 rounded-lg flex flex-col justify-center items-center">
+                    <h2 className="text-center mb-1 text-xl">
+                      Import from MyAnimeList?
+                    </h2>
+                    <p className="text-center text-xs">
+                      (MAL account must be public)
+                    </p>
+                    <div className="flex-auto flex justify-between">
+                      <label htmlFor="username" className="text-lg m-2">
+                        MAL Username
+                      </label>
+                      <input
+                        id="username"
+                        type="text"
+                        value={username}
+                        className="flex-grow border-solid border-4 m-2 p-1 rounded focus:outline-none focus:shadow-outline"
+                        onChange={(event) => setUsername(event.target.value)}
+                        onKeyPress={onKeyPressImportFromMal}
+                      />
+                    </div>
+
+                    <button
+                      type="button"
+                      className="m-2 max-w-xs bg-green-800 hover:bg-green-700 text-white text-lg py-2 px-4 rounded flex-grow-0"
+                      onClick={() => importFromMal()}
+                    >
+                      Import
+                    </button>
+                  </div>
+                </>
+              ) : (
+                <div
+                  className={
+                    listHasUnsavedChanges
+                      ? 'm-1 p-1 border rounded border-red-700'
+                      : ''
+                  }
+                >
+                  {shows.map((show) => (
+                    <DraggableRankedShow
+                      key={show.name}
+                      show={show}
+                      updator={deleteShow}
+                      moveToTop={moveToTop}
+                      moveToBottom={moveToBottom}
+                    />
+                  ))}
+                </div>
+              )}
+              {provided.placeholder}
+            </main>
+          </>
         )}
       </Droppable>
     </DragDropContext>
