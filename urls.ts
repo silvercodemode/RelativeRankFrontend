508e7543b30665403d74c565c361ba6b8708e3cf
Martin <martinpayne73@gmail.com>
2020-07-11 17:38:58 -0400

    change api base url

--- urls.ts
+++ urls.ts
@@ -1,4 +1,4 @@
-const baseUrl = 'https://relative-rank-api.azurewebsites.net';
+const baseUrl = 'http://67.205.136.220:5000';
 
 export const relativeRankedShowsEndpoint = `${baseUrl}/index`;
 export const signUpEndpoint = `${baseUrl}/user/signup`;
